cedb26c242c85e0dd2de641eb38f62b4225fef89
Znackt <znackt@proton.me>
2025-07-30 23:25:57 +0530

    added changes

--- Brainly-F/src/config.ts
+++ Brainly-F/src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:5050";
-export const FRONTEND_URL = "http://localhost:5173";
+export const BACKEND_URL = "https://brainz-lhoj.onrender.com/";
+export const FRONTEND_URL = "https://brainz-silk.vercel.app/";
--- Brainly-F/vite.config.ts
+++ Brainly-F/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:5050",
+        target: "https://brainz-lhoj.onrender.com/",
         changeOrigin: true,
         secure: false
       }
